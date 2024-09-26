ad042d360a5d93eeed4c332086221182e452102f
Trang Tung Nguyen <trangtungn@gmail.com>
2024-09-26 13:25:17 +0700

    add correct solutions using DFS and BFS for task_three

--- challenges/colidity/employment_hero/task_three.rb
+++ challenges/colidity/employment_hero/task_three.rb
@@ -4,6 +4,8 @@
 require 'set'
 
 def solution(connections, name1, name2)
+  return 0 if name1 == name2
+  return -1 if connections.empty?
   graph = Hash.new { |h, k| h[k] = [] }
 
   # Build the graph
@@ -17,7 +19,7 @@ def solution(connections, name1, name2)
   queue = [[name1, 0]]
   visited = Set.new([name1])
 
-  while !queue.empty?
+  until queue.empty?
     name, distance = queue.shift
     return distance if name == name2
 
@@ -33,8 +35,10 @@ def solution(connections, name1, name2)
   -1
 end
 
+# Solution 2: using hash. failed test case 4, 5, 6, 8 (index 3).
 def solution2(connections, name1, name2)
   return -1 if connections.empty?
+  return 0 if name1 == name2
 
   if connections.size == 1
     return 0 if connections.first.include?(name1) && connections.first.include?(name2)
@@ -71,15 +75,103 @@ def solution2(connections, name1, name2)
   -1
 end
 
+# Solution 3: using BFS. passed all tests
+# Breadth First Search or BFS for a Graph: https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/#applications-of-bfs-in-graphs
+def solution3(connections, head, tail)
+  return -1 if connections.empty?
+
+  mapper = {}
+  connections.each do |connection|
+    return 0 if connection.include?(head) && connection.include?(tail)
+
+    name1, name2 = connection.split(':')
+    mapper[name1] = [] unless mapper[name1]
+    mapper[name1] << name2
+  end
+
+  p mapper
+
+  queue = [head]
+  visited = { head => true }
+  count = -1
+  reached = false
+  until queue.empty?
+    item = queue.shift
+    next if !mapper[item]
+
+    mapper[item].each do |next_item|
+      next if visited[next_item]
+
+      if next_item == tail
+        reached = true
+        break
+      end
+
+      visited[next_item] = true
+      queue << next_item
+    end
+
+    count += 1
+  end
+
+  reached ? count : -1
+end
+
+# Solution 4: using DFS. passed all tests.
+# Depth First Search or DFS for a Graph: https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
+def solution4(connections, head, tail)
+  return -1 if connections.empty?
+
+  mapper = {}
+  connections.each do |connection|
+    return 0 if connection.include?(head) && connection.include?(tail)
+
+    from, to = connection.split(':')
+    mapper[from] = [] unless mapper[from]
+    mapper[from] << to
+  end
+
+  p "mapper"
+  p mapper
+
+  stack = [head]
+  visited = { head => -1 }
+  while stack.any?
+    # p "stack"
+    # p stack
+    item = stack.pop
+    next_items = mapper[item]
+    next if !next_items
+
+    next_items.each do |next_item|
+      return visited[item] + 1 if next_item == tail
+      next if visited[next_item]
+
+      stack << next_item
+      visited[next_item] = visited[item] + 1
+    end
+  end
+
+  -1
+end
+
 args = [
   [['fred:joe', 'joe:mary', 'mary:fred', 'mary:bill'], 'fred', 'bill'],
   [['fred:joe', 'joe:mary', 'kate:sean', 'sean:sally'], 'fred', 'sally'],
   [['fred:joe', 'joe:sally'], 'fred', 'sally'],
   [['fred:joe', 'joe:sean', 'joe:mary', 'kate:sean', 'sean:sally'], 'fred', 'sally'],
+  [['fred:joe', 'joe:mary', 'kate:sean', 'sean:sally', 'mary:bob', 'bob:sean', 'bob:bill'], 'fred', 'sally'],
+  [['fred:joe', 'joe:mary', 'mary:fred', 'mary:bill'], 'mary', 'fred'],
+  [['fred:joe', 'joe:mary', 'mary:fred', 'mary:mary'], 'mary', 'mary'],
+  [['fred:joe', 'joe:mary', 'mary:fred', 'mary:mary', 'kate:sean', 'sean:sally', 'mary:bob', 'bob:sean', 'bob:bill'], 'fred', 'sally'],
+  [[], 'fred', 'sally']
 ]
 
 args.each do |arg|
-  p '-' * 20
+  p "=" * 20
+  p "case: #{arg}"
   p "#1: #{solution(*arg)}"
   p "#2: #{solution2(*arg)}"
+  p "#3: #{solution3(*arg)}"
+  p "#4: #{solution4(*arg)}"
 end
