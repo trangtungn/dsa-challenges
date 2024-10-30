c9aa7c6266468fc6f241fd4bc691e84ea32818de
Trang Tung Nguyen <trangtungn@gmail.com>
2024-10-30 12:03:26 +0700

    improve task three

--- challenges/colidity/employment_hero/task_three.rb
+++ challenges/colidity/employment_hero/task_three.rb
@@ -1,81 +1,7 @@
 #!/Users/trangtungn/.rbenv/shims/ruby -w
 # frozen_string_literal: true
 
-require 'set'
-
-def solution(connections, name1, name2)
-  return 0 if name1 == name2
-  return -1 if connections.empty?
-  graph = Hash.new { |h, k| h[k] = [] }
-
-  # Build the graph
-  connections.each do |connection|
-    name_a, name_b = connection.split(":")
-    graph[name_a] << name_b
-    graph[name_b] << name_a
-  end
-
-  # Perform a breadth-first search to find the shortest path between the two names
-  queue = [[name1, 0]]
-  visited = Set.new([name1])
-
-  until queue.empty?
-    name, distance = queue.shift
-    return distance if name == name2
-
-    graph[name].each do |friend|
-      next if visited.include?(friend)
-
-      visited << friend
-      queue << [friend, distance + 1]
-    end
-  end
-
-  # If we've searched the entire graph and haven't found a path to the target name, return -1
-  -1
-end
-
-# Solution 2: using hash. failed test case 4, 5, 6, 8 (index 3).
-def solution2(connections, name1, name2)
-  return -1 if connections.empty?
-  return 0 if name1 == name2
-
-  if connections.size == 1
-    return 0 if connections.first.include?(name1) && connections.first.include?(name2)
-
-    -1
-  end
-
-  con_mapper = {}
-  connections.each do |con|
-    head, tail = con.split(':')
-
-    con_mapper[head] = tail
-  end
-
-  count = 0
-  match = false
-  first = name1
-  while con_mapper.any?
-    last = con_mapper.delete(first)
-    return -1 if last.nil? || last.empty?
-
-    count += 1
-
-    if con_mapper[last] == name2
-      match = true
-      break
-    end
-
-    first = last
-  end
-
-  return count if match == true
-
-  -1
-end
-
-# Solution 3: using BFS. passed all tests
+# Solution 3: using BFS. PASSED ALL TESTS
 # Breadth First Search or BFS for a Graph: https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/#applications-of-bfs-in-graphs
 def solution3(connections, head, tail)
   return -1 if connections.empty?
@@ -116,7 +42,7 @@ def solution3(connections, head, tail)
   reached ? count : -1
 end
 
-# Solution 4: using DFS. passed all tests.
+# Solution 4: using DFS. passed all tests ***BUT NOT THE SHORTEST PATH***
 # Depth First Search or DFS for a Graph: https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
 def solution4(connections, head, tail)
   return -1 if connections.empty?
@@ -129,7 +55,6 @@ def solution4(connections, head, tail)
     mapper[from] << to
   end
 
-  p "mapper"
   p mapper
 
   stack = [head]
@@ -167,10 +92,7 @@ args = [
 ]
 
 args.each do |arg|
-  p "=" * 20
-  p "case: #{arg}"
-  p "#1: #{solution(*arg)}"
-  p "#2: #{solution2(*arg)}"
-  p "#3: #{solution3(*arg)}"
-  p "#4: #{solution4(*arg)}"
+  p "======= Case: #{arg}"
+  p "#3 - BFS: #{solution3(*arg)}"
+  p "#4 - DFS: #{solution4(*arg)}"
 end
